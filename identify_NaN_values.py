fad758a20fa92f4f2bd4ff2eeec4940fc6488b5a
r3card0 <ricardo.rdz@pm.me>
2022-09-06 23:06:13 -0500

    This program returns all rows with NaN values, if not, returns Dataframe free of NaN values mesage

--- identify_NaN_values.py
+++ identify_NaN_values.py
@@ -5,14 +5,43 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from read_dataframe import read_dataframe
 
+def description():
+    program_description = """
+    This program returns all rows
+    with NaN values, if not, returns
+    Dataframe free of NaN values  """
+    return program_description
+
 df = read_dataframe()
 
 def identify_nan():
     value_null = df.isnull().values.any()
     return value_null
 
+value_null = identify_nan()
+
+def rows_nan():
+    rows_value_null = df[df.isnull().any(axis=1)]
+    return rows_value_null
+
+def free_nan():
+    free_of_null_values = "Dataframe free of NaN values"
+    return free_of_null_values
+
+# Variables
+value_null = identify_nan()
+rows_value_null = rows_nan()
+free_of_null_values = free_nan()
+
+def null_values():
+    if value_null == True:
+        return rows_value_null
+    else:
+        return free_of_null_values
+
 def run():
-    print(identify_nan())
+    print(description())
+    print(null_values())
 
 
 if __name__ == "__main__":
